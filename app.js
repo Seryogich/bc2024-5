5340a7a944b0a58a40e63ee2d98b7306301d49c3
Seryogich bs2024-5 <SERHII.PROTSAK@lnu.edu.ua>
2024-11-06 13:54:34 +0200

    was added Post and Delete method

--- app.js
+++ app.js
@@ -24,49 +24,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-// app.get('/notes/:noteName', (req, res) => {
-//   const note = notes.find(n => n.name === req.params.noteName);
-//   if (!note) {
-//     return res.status(404).send('Not found');
-//   }
-//   res.send(note.text);
-// });
+app.get('/notes/:noteName', (req, res) => {
+  const note = notes.find(n => n.name === req.params.noteName);
+  if (!note) {
+    return res.status(404).send('Not found');
+  }
+  res.send(note.text);
+});
 
 
-// app.put('/notes/:noteName', (req, res) => {
-//   const note = notes.find(n => n.name === req.params.noteName);
-//   if (!note) {
-//     return res.status(404).send('Not found');
-//   }
-//   note.text = req.body.text; 
-//   res.send('Note updated');
-// });
+app.put('/notes/:noteName', (req, res) => {
+  const note = notes.find(n => n.name === req.params.noteName);
+  if (!note) {
+    return res.status(404).send('Not found');
+  }
+  note.text = req.body.text; 
+  res.send('Note updated');
+});
 
 
-// app.delete('/notes/:noteName', (req, res) => {
-//   const index = notes.findIndex(n => n.name === req.params.noteName);
-//   if (index === -1) {
-//     return res.status(404).send('Not found');
-//   }
-//   notes.splice(index, 1);
-//   res.send('Note deleted');
-// });
+app.delete('/notes/:noteName', (req, res) => {
+  const index = notes.findIndex(n => n.name === req.params.noteName);
+  if (index === -1) {
+    return res.status(404).send('Not found');
+  }
+  notes.splice(index, 1);
+  res.send('Note deleted');
+});
 
 
-// app.get('/notes', (req, res) => {
-//   res.json(notes);
-// });
+app.get('/notes', (req, res) => {
+  res.json(notes);
+});
 
 
-// app.post('/write', (req, res) => {
-//   const { note_name, note } = req.body;
-//   const existingNote = notes.find(n => n.name === note_name);
-//   if (existingNote) {
-//     return res.status(400).send('Bad Request');
-//   }
-//   notes.push({ name: note_name, text: note });
-//   res.status(201).send('Created');
-// });
+app.post('/write', (req, res) => {
+  const { note_name, note } = req.body;
+  const existingNote = notes.find(n => n.name === note_name);
+  if (existingNote) {
+    return res.status(400).send('Bad Request');
+  }
+  notes.push({ name: note_name, text: note });
+  res.status(201).send('Created');
+});
 
 
 // app.get('/UploadForm.html', (req, res) => {
